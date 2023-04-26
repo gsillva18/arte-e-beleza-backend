ff96156b0e1833f7627897392f6e178f6d63e34a
José Gabriel da Silva Lima <silva.jose@academico.ifpb.edu.br>
2023-04-26 16:29:15 -0300

    creation of art and beauty

---
+++ arby/src/main/java/com/artbeauty/arby/ArbyApplication.java
@@ -0,0 +1,13 @@
+package com.artbeauty.arby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArbyApplication.class, args);
+    }
+
+}
---
+++ arby/src/test/java/com/artbeauty/arby/ArbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artbeauty.arby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
